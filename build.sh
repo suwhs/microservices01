1da652a5ada346b525e386cc06ca023eb8480864
igor boulliev <igor@whs.su>
2023-04-17 19:26:18 +0300

    fix old path

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-cd build/service1/
+cd commands/service1/
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -tags netgo -ldflags '-w'
 minikube image build -t msvc/service-1
 
 cd ../../
-cd build/service2
+cd commands/service2
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -tags netgo -ldflags '-w'
 minikube image build -t msvc/service-2
 
